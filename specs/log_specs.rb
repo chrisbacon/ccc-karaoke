8c2f4de2a38bb4e4b666255a2644c5fa25f292c7
chrisbacon <bacon.chris@gmail.com>
2016-11-11 21:39:20 +0000

    created some tests for venue class

---
+++ specs/log_specs.rb
@@ -0,0 +1,11 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../log')
+
+class Testlog < Minitest::Test
+
+    def setup
+
+    end
+
+end
---
+++ specs/venue_specs.rb
@@ -0,0 +1,27 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative("../venue")
+require_relative("../room")
+
+class TestVenue < Minitest::Test
+
+    def setup
+        log = []
+        machine = {}
+        @guest1 = Person.new("Chris", 30, 100)
+        @guest2 = Person.new("Todd", 29, 80)
+
+        @room1 = Room.new('Classic', 30, 1, {})
+        @room.add_guest(@guest1)
+
+        @room2 = Room.new('Modern', 30, 1, {})
+        rooms = [@room1, @room2]
+
+        @venue = Venue.new(rooms, log)
+    end
+
+    def test_find_available_rooms
+        assert_equal([@room2], @venue.find_available_rooms())
+    end
+
+end
