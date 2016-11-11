21d3568792429d8f970823c97df86cf374eb851c
chrisbacon <bacon.chris@gmail.com>
2016-11-11 16:20:38 +0000

    added and tested get song titles by artist method

--- jukebox.rb
+++ jukebox.rb
@@ -10,5 +10,17 @@ class Jukebox
     def add_song(song)
         @songs.push(song)
     end
+
+    def get_song_titles_by_artist()
+
+        songs_by_artist = Hash.new([])
+
+        for song in @songs
+            songs_by_artist[song.artist] += [song.title]
+        end
+
+        return songs_by_artist
+
+    end
     
 end
--- specs/jukebox_specs.rb
+++ specs/jukebox_specs.rb
@@ -8,6 +8,7 @@ class TestJukebox < Minitest::Test
     def setup
         @song1 = Song.new('Leonard Cohen', 'The Stranger Song', ["It's true that all the men you knew were dealers"])
         @song2 = Song.new('Prince', 'Raspberry Beret', ["I was working part time at a five and dime", "my boss was Mr. McGee"])
+        @song3 = Song.new("Prince", "Let's Go Crazy", ["Dearly beloved", "we are gathered here today to get through this thing called life"])
 
         songs = [@song1]
 
@@ -23,5 +24,18 @@ class TestJukebox < Minitest::Test
         @jukebox.add_song(@song2)
         assert_equal(2, @jukebox.number_of_songs)
     end
-    
+
+    def test_get_songs_by_artist
+        @jukebox.add_song(@song2)
+        @jukebox.add_song(@song3)
+
+        songs_by_artist = {
+            "Leonard Cohen" => ["The Stranger Song"] ,
+            "Prince" => ["Raspberry Beret", "Let's Go Crazy"]
+                    }
+
+        assert_equal(songs_by_artist, @jukebox.get_song_titles_by_artist)
+    end
+
+
 end
